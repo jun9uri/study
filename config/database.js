66a6bf200af61585558e0bc9b108e59bcb67ee74
jun9uri <ibevery@naver.com>
2020-09-20 18:11:51 +0900

    commit

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('promise-mysql');
 
 const connection = {
-    host: 'localhost',  // 로컬 호스트 주소
-    port: 3306,         // 포트번호
-    user: 'root',      // 유저
-    password: '7457',   // 비밀번호
+    host: process.env.DB_HOST,  // 로컬 호스트 주소
+    port: process.env.DB_PORT,         // 포트번호
+    user: process.env.DB_NAME,      // 유저
+    password: process.env.DB_PASSWORD,   // 비밀번호
     database: 'hyunjung'   // 접근할 데이터베이스 (스키마)
 };
 
